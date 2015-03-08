aa7cb2823d2ec13da6c9ea1d3789d7e42f6aba04
aitorpena8 <aitorpena@gmail.com>
2015-03-08 14:04:10 +0100

    dom1

--- Ejercicios/js/dom/dom1/dom.js
+++ Ejercicios/js/dom/dom1/dom.js
@@ -1,3 +1,42 @@
-document.getElementsByTagNam("a");
-document.createElement("p");
-document.body.appendChild();
+var links = document.getElementsByTagName("a");
+var linksNumb = links.length;
+var outString = "Número de enlances: " + linksNumb;
+var p = document.createElement("p");
+var textNode = document.createTextNode(outString);
+p.appendChild(textNode);
+document.body.appendChild(p);
+
+
+var link = links[linksNumb - 2];
+var outString = "Dirección penúltimo enlace: " + link.href;
+var p = document.createElement("p");
+var textNode = document.createTextNode(outString);
+p.appendChild(textNode);
+document.body.appendChild(p);
+
+var sComp = 'http://prueba/';
+
+var cont = 0;
+for (var i in links) {
+    if (links[i].href == sComp)
+        cont++;
+}
+
+
+var outString = "Número de enlaces a "+sComp+": " + cont;
+var p = document.createElement("p");
+var textNode = document.createTextNode(outString);
+p.appendChild(textNode);
+document.body.appendChild(p);
+
+
+var paragraphs = document.getElementsByTagName("p");
+
+var paragraph = paragraphs[2];
+var links3p = paragraph.getElementsByTagName("a");
+
+var outString = "Número de enlaces del 3. parrafo: " + links3p.length;
+var p = document.createElement("p");
+var textNode = document.createTextNode(outString);
+p.appendChild(textNode);
+document.body.appendChild(p);
