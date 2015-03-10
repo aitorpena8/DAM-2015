d085a2bb2f445a5a5b36c438f79f0ead1b2395dd
unknown <FPE@965PDS3-005.aeg8.edu>
2015-03-10 13:44:09 +0100

    ajax ejerc1

--- Ejercicios/js/ajax/ej01/code.js
+++ Ejercicios/js/ajax/ej01/code.js
@@ -1,15 +1,22 @@
+"use strict";
 var READY_STATE_UNINITIALIZED = 0;
 var READY_STATE_LOADING = 1;
 var READY_STATE_LOADED = 2;
 var READY_STATE_INTERACTIVE = 3;
 var READY_STATE_COMPLETE = 4;
 
-var estados=["No Inicializada","Cargando","Cargado","Interactivo","Completada"];
+var estados = ["No Inicializada", "Cargando", "Cargado", "Interactivo", "Completada"];
 
 var peticion_http;
+var peticion_date;
 
 function cargaContenido(url, metodo, funcion) {
+    document.getElementById("estados").innerHTML = "";
+    document.getElementById("codigo").innerHTML = "";
+    document.getElementById("contenidos").innerHTML = "";
+    document.getElementById("cabeceras").innerHTML = "";
     peticion_http = inicializa_xhr();
+    peticion_date = new Date();
 
     if (peticion_http) {
         peticion_http.onreadystatechange = funcion;
@@ -28,13 +35,21 @@ function inicializa_xhr() {
 
 function muestraContenido() {
 
-    document.getElementById("estados").innerHTML = peticion_http.readyState;
-    document.getElementById("codigo").innerHTML = peticion_http.readyState;
+    var current_date = new Date();
+    var dif = new Date(current_date.getTime() - peticion_date.getTime());
+    var elapsedStr = dif.getSeconds() + ":" + dif.getMilliseconds();
+
+    document.getElementById("estados").innerHTML += "[" + elapsedStr + "]" + estados[peticion_http.readyState] + "</br>";
 
     if (peticion_http.readyState == READY_STATE_COMPLETE) {
         if (peticion_http.status == 200) {
-            document.getElementById("contenidos").innerHTML=peticion_http.responseText;
-            document.getElementById("cabeceras").innerHTML=peticion_http.getAllResponseHeaders();
+            document.getElementById("codigo").innerHTML = peticion_http.status + "</br>" + peticion_http.statusText;
+            document.getElementById("contenidos").innerHTML = peticion_http.responseText;
+            document.getElementById("cabeceras").innerHTML = peticion_http.getAllResponseHeaders();
+
+        } else {
+            document.getElementById("codigo").innerHTML = peticion_http.status + "</br>" + peticion_http.statusText;
+            document.getElementById("cabeceras").innerHTML = peticion_http.getAllResponseHeaders();
 
         }
     }
@@ -46,13 +61,13 @@ function rellenarRecurso() {
 }
 
 function descargaArchivo() {
-    cargaContenido("http://localhost/vegiesipsum.txt", "GET", muestraContenido);
+    var file = "vegiesipsum.txt"
+    var url = document.getElementById("recurso").value + file;
+    cargaContenido(url, "GET", muestraContenido);
 }
 
-document.addEventListener("load", function () {
+window.onload = function () {
     rellenarRecurso();
     var enviar = document.getElementById("enviar");
     enviar.addEventListener("click", descargaArchivo, false);
-
-}, false);
-
+};
